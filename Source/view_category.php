97385a50e65ae0c38c856665a508d05e52bb39d4
Jasper De Moor <jdemo@live.be>
2014-04-11 18:15:25 +0200

    Image BugFix
    Bug fixed with image
    Images for topics loading now :)

--- Source/view_category.php
+++ Source/view_category.php
@@ -1,8 +1,5 @@
 <?php
-session_start(); // Start your sessions to allow your page to interact with session variables
-
 include_once("header.php");
-
 // Connect to the database
 include_once("connect.php");
 
@@ -30,11 +27,11 @@ if (isset($_SESSION['uid'])) {
     $logged = " You have to be signed in to make a topic.";
 }
 
-function hot($views) {
+function hot($views, $Root, $getTheme) {
     if ($views > "100"){
-return "<img src='img/hot_topic.png' style='' />";
+		return "<img src='".$Root."Themes/".$getTheme."/img/hot_topic.png' style='' />";
     } elseif ($views < "100") {
-return "<img src='img/normal_topic.png' style='' />";
+		return "<img src='".$Root."Themes/".$getTheme."/img/normal_topic.png' style='' />";
 }
 }
 
@@ -75,7 +72,7 @@ if (mysql_num_rows($res) == 1) {
             // Check om te zien of iemand ooit al een reply gedaan heeft
             if ($row['topic_last_user'] == "") { $last_user = "N/A"; } else { $last_user = getusername($row['topic_last_user']); }
             // Append the actual topic data to the $topics variable
-            $topics .= "<tr><td>".hot($row['topic_views'])."<td><a href='view_topic.php?cid=".$cid."&tid=".$tid."'>".$title."</a><br /><span class='post_info'>Posted by: ".getusername($creator)." on ".convertdate($date)."</span></td><td align='center' style=''>".$last_user."</td><td>replies:<br />views:</td><td>".topic_replies($cid, $tid)." <br /> ".$views."</td></tr>";
+            $topics .= "<tr><td>".hot($row['topic_views'], $Root, $getTheme)."<td><a href='view_topic.php?cid=".$cid."&tid=".$tid."'>".$title."</a><br /><span class='post_info'>Posted by: ".getusername($creator)." on ".convertdate($date)."</span></td><td align='center' style=''>".$last_user."</td><td>replies:<br />views:</td><td>".topic_replies($cid, $tid)." <br /> ".$views."</td></tr>";
             }
         $topics .= "</tbody>
             </table>
@@ -88,7 +85,7 @@ $rs_result = mysql_query($sql2) or die(mysql_error());
 $row = mysql_num_rows($rs_result);
 $total_pages = ceil($row / 20); 
 
-echo "Pagina ";
+echo "Page ";
 for ($i=1; $i<=$total_pages; $i++) { 
             echo "<a href='?cid=".$cid."&page=".$i."'>".$i."</a> "; 
 };
@@ -98,9 +95,9 @@ for ($i=1; $i<=$total_pages; $i++) {
         echo "<p>There are no topics in this category yet.".$logged."</p>";
     }
 	
-	echo "<br /><img src='img/hot_topic.png' style='height:20px;width:20px;' /> = Famous Topic 
-	<img src='img/normal_topic.png' style='height:20px;width:20px;' /> = Normal Topic
-	<img src='img/sticky.png' style='height:20px;width:20px;' /> = Sticky Topic";
+	echo "<br /><img src='".$Root."/Themes/".$getTheme."/img/hot_topic.png' style='height:20px;width:20px;' /> = Hot Topic 
+	<img src='".$Root."/Themes/".$getTheme."/img/normal_topic.png' style='height:20px;width:20px;' /> = Normal Topic
+	<img src='".$Root."/Themes/".$getTheme."/img/sticky.png' style='height:20px;width:20px;' /> = Sticky Topic";
 } else {
 	// If the category does not exist
 	echo "<h1>404</h1>
