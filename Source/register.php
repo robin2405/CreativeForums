a41779434667638438c0f270c8b90230feaec4ff
Jasper De Moor <jdemo@live.be>
2014-06-01 17:16:24 +0200

    Captcha bugfix

--- Source/register.php
+++ Source/register.php
@@ -19,18 +19,22 @@
 	$Root = getRoot();
 
 	include_once(dirname(__FILE__)."/Themes/".$getTheme."/RegisterHeader.php");
+	$CurrentUrl = $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	$CaptchaUrl = str_replace("register.php", "", $CurrentUrl) . "securimage/securimage_show.php?";
+	$captcha = "captcha";
+
+	echo '
+	<div class="container">
+
+	    <form action="register_parse.php" method="post" class="form-signin" role="form">
+	        <h2 class="form-signin-heading">Register</h2>
+	        <input type="username" name="username" class="form-control" placeholder="Username" required autofocus>
+	        <input type="email" name="email" class="form-control" placeholder="email" required>
+			<input type="password" name="password" class="form-control" placeholder="Password" required>
+			<center><img id="captcha" src="http://localhost/Creative%20Forums/securimage/securimage_show.php" /><br />
+			<input type="text" class="form-control" name="captcha_code" size="10" maxlength="6" style="width:50%;" />
+			<a href="#" onclick="document.getElementById('.$captcha.').src = '.$CaptchaUrl.' + Math.random(); return false">[ Different Image ]</a></center>
+			<br /><button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Register</button>
+		</form>
+	</div> ';
 ?>
-
-<div class="container">
-
-    <form action="register_parse.php" method="post" class="form-signin" role="form">
-        <h2 class="form-signin-heading">Register</h2>
-        <input type="username" name="username" class="form-control" placeholder="Username" required autofocus>
-        <input type="email" name="email" class="form-control" placeholder="email" required>
-		<input type="password" name="password" class="form-control" placeholder="Password" required>
-		<center><img id="captcha" src="http://localhost/Creative%20Forums/securimage/securimage_show.php" /><br />
-		<input type="text" class="form-control" name="captcha_code" size="10" maxlength="6" style="width:50%;" />
-		<a href="#" onclick="document.getElementById('captcha').src = 'http://localhost/Creative%20Forums/securimage/securimage_show.php?' + Math.random(); return false">[ Different Image ]</a></center>
-		<br /><button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Register</button>
-	</form>
-</div>
